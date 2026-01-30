68e22a189e56d3256fc7ca4fe5aba4ba41f0fae6
vfazzanaro <vfazzanaro@gmail.com>
2026-01-30 17:25:03 -0300

    added comparison operator

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ function generateNumber() {
    const min = Math.ceil(document.querySelector('.input-min').value);
    const max = Math.floor(document.querySelector('.input-max').value);
    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-   document.getElementById("resultado").innerHTML = `Número sorteado: <strong>${result}</strong>`;
+   if (min > max) {
+      alert("O valor mínimo deve ser menor que o valor máximo.");
+   } else {
+      document.getElementById("resultado").innerHTML = `Número sorteado: <strong>${result}</strong>`;
+   }
 }
